1043f4f76a2367c4b9504f05a491ff3276e81782
tinkerbells <daniel.kasatkin.work@gmail.com>
2026-01-11 18:13:16 +0300

    feat(mobx): add Solid bridge and dependency

--- src/renderer/src/main.tsx
+++ src/renderer/src/main.tsx
@@ -1,9 +1,12 @@
 import { render } from 'solid-js/web'
 
 import App from './App'
+import { initMobxObserver } from './infrastructure/mobx/init-mobx'
 
 import 'uno.css'
 
 import './assets/main.css'
+
+initMobxObserver()
 
 render(() => <App />, document.getElementById('root') as HTMLElement)
