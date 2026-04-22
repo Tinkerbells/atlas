2d534bbf8bb9dc965548a35e125e271ea6b7abda
tinkerbells <daniel.kasatkin.work@gmail.com>
2026-04-22 15:47:22 +0300

    refactor(shell): remove unnecessary `as any` casts from registerSingleton calls

--- apps/shell/src/main.ts
+++ apps/shell/src/main.ts
@@ -31,14 +31,14 @@ import "./style.css";
 import App from "./app.vue";
 import { InstantiationServiceKey } from "./injection-keys";
 
-registerSingleton(ILogger, ConsoleLogger as any, 1);
-registerSingleton(ICommandRegistry, CommandRegistry as any, 0);
-registerSingleton(ICommandService, CommandService as any, 0);
-registerSingleton(IContextKeyService, ContextKeyService as any, 0);
-registerSingleton(IKeypressEventBus, KeypressEventBus as any, 0);
-registerSingleton(IKeybindingsRegistry, KeybindingsRegistryImpl as any, 0);
-registerSingleton(IKeyboardLayoutService, BrowserKeyboardLayoutService as any, 1);
-registerSingleton(IKeybindingService, KeybindingService as any, 1);
+registerSingleton(ILogger, ConsoleLogger, 1);
+registerSingleton(ICommandRegistry, CommandRegistry, 0);
+registerSingleton(ICommandService, CommandService, 0);
+registerSingleton(IContextKeyService, ContextKeyService, 0);
+registerSingleton(IKeypressEventBus, KeypressEventBus, 0);
+registerSingleton(IKeybindingsRegistry, KeybindingsRegistryImpl, 0);
+registerSingleton(IKeyboardLayoutService, BrowserKeyboardLayoutService, 1);
+registerSingleton(IKeybindingService, KeybindingService, 1);
 
 const descriptors = getSingletonServiceDescriptors();
 const serviceCollection = new ServiceCollection(...descriptors);
